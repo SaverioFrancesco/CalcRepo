8a29d1eb5a2269a1fbbadfd7826e3736d9b1f890
Francesco Saverio Comisso <francescosaverio.comisso@beantech.it>
2019-03-19 12:20:16 +0100

    diff

--- src/Program.cs
+++ src/Program.cs
@@ -76,3 +76,53 @@ public class SumParserTest
 }
 
 public class SumOperation : IOperation { public decimal Eval() { throw new System.NotImplementedException(); } }
+
+
+public class DiffTest
+{
+    private readonly UserInputParser _sut;
+    public DiffTest() { _sut = new UserInputParser(); }
+    [Theory(DisplayName = "Deve gestire le sottrazioni")]
+    [InlineData("34-8", true, 26)]
+    [InlineData("a-1", false, 0)]
+    [InlineData(null, false, 0)]
+    public void Parse_should_support_diffs(string input, bool canParse, decimal expected)
+    {
+        IOperation operation;
+        var res = _sut.TryParse(input, out operation);
+        res.Should().Be(canParse); if (canParse)
+        {
+            operation.Should().Be.OfType<DiffOperation>();
+            operation.Eval().Should().Be.EqualTo(expected);
+        }
+        else
+        {
+            operation.Should().Be.Null();
+        }
+    }
+}
+
+public class UserInputParser : ICalculatorParser
+{
+    public bool TryParse(string userInput, out IOperation request)
+    {
+        if (IsDiff(userInput)) { request = ParseDiff(userInput); return true; }
+        request = null; return false;
+    }
+    private static IOperation ParseDiff(string userInput) {
+        var parts = userInput.Split(new[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+        var a = int.Parse(parts.ElementAt(0));
+        var b = int.Parse(parts.ElementAt(1));
+        return new DiffOperation(a, b);
+    }
+    private static bool IsDiff(string userInput) {
+        return userInput != null && Regex.IsMatch(userInput, @"^[0-9]+\-[0-9]+$");
+    }
+}
+
+public class DiffOperation : IOperation
+{
+    private readonly int _a; private readonly int _b;
+    public DiffOperation(int a, int b) { _a = a; _b = b; }
+    public decimal Eval() { return _a - _b; }
+}
